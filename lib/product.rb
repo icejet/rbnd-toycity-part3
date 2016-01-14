d82618ee93f2ee0d8809c0fc758c07046378bb17
James Teitsworth <james.teitsworth@outlook.com>
2016-01-13 21:33:04 -0600

    Add ability to throw an error on duplicate items.

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,28 @@
 class Product
+  attr_reader :title
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    product_exists = false
+    @@products.each do |product|
+      product_exists = true if product.title == @title
+    end
+    if product_exists
+      raise DuplicateProductError, "#{@title} already exists."
+    else
+      @@products << self
+    end
   end
 end
