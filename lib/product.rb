b7c36b4d197a7aa4a021da9fe4eb003cec8dc648
James Teitsworth <james.teitsworth@outlook.com>
2016-01-14 10:28:47 -0600

    Add methods to search and check products.

--- lib/product.rb
+++ lib/product.rb
@@ -1,15 +1,29 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options={})
     @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
     add_to_products
+  end
+
+  def in_stock?
+    @stock > 0
   end
 
   def self.all
     @@products
+  end
+
+  def self.find_by_title(title)
+    @@products.at(@@products.index { |product| product.title == title })
+  end
+
+  def self.in_stock
+    @@products.select { |product| product.stock > 0 }
   end
 
   private
