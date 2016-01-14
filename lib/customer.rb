895f1fb2a791ee48ee68924240ee7b7ba88454eb
James Teitsworth <james.teitsworth@outlook.com>
2016-01-14 13:58:25 -0600

    Add customer features.

---
+++ lib/customer.rb
@@ -0,0 +1,32 @@
+class Customer
+  attr_reader :name
+
+  @@customers = []
+
+  def initialize(options={})
+    @name = options[:name]
+    add_to_customers
+  end
+
+  def self.all
+    @@customers
+  end
+
+  def self.find_by_name(name)
+    @@customers.at(@@customers.index { |customer| customer.name == name })
+  end
+
+  private
+
+  def add_to_customers
+    customer_exists = false
+    @@customers.each do |customer|
+      customer_exists = true if customer.name == @name
+    end
+    if customer_exists
+      raise DuplicateCustomerError, "'#{@name}' already exists"
+    else
+      @@customers << self
+    end
+  end
+end
